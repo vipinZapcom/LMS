8715417603bdfbb3de410c7ba53f6cb0005975bd
vipinZapcom <vipin.dixit@zapcg.com>
2025-08-20 14:16:10 +0530

    group test cases.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const user = require("../models/user");
+require('dotenv').config();
 
 // =========================
 // POST /api/login/user/signup
@@ -56,7 +58,7 @@ exports.login = async (req, res) => {
       { expiresIn: "10h" }
     );
 
-    res.json({ accessToken: token, id: user._id });
+    res.status(200).json({ message: "Login successful", accessToken: token, id: user._id });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -67,16 +69,23 @@ exports.login = async (req, res) => {
 // =========================
 exports.getUserById = async (req, res) => {
   try {
-    const user = await User.findOne({
-      _id: req.params.id,
-      isActive: true,
-    }).select("-password");
+    const queryField = req.params.id.includes("@") ? "email" : "_id";
+    const user = await User.findOne({ [queryField]: req.params.id, isActive: true })
+console.log(`user`);
+console.log(user);
+
     if (!user) return res.status(404).json({ message: "User not found" });
-    res.json(user);
+
+    res.status(200).json(user);
   } catch (err) {
+    console.log(`inside catch`);
+    
+    console.log(err.message);
+    
     res.status(500).json({ message: err.message });
   }
 };
+
 
 // =========================
 // PUT /api/user/update/:id
@@ -98,7 +107,7 @@ exports.updateUser = async (req, res) => {
     if (!updatedUser)
       return res.status(404).json({ message: "User not found" });
 
-    res.json({
+    res.status(200).json({
       message: "User updated successfully",
       user: {
         id: updatedUser._id,
@@ -125,7 +134,7 @@ exports.deactivateUser = async (req, res) => {
     if (!updatedUser)
       return res.status(404).json({ message: "User not found" });
 
-    res.json({ message: "User deactivated successfully" });
+    res.status(200).json({ message: "User deactivated successfully" });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -135,19 +144,10 @@ exports.deactivateUser = async (req, res) => {
 // GET /api/user/detail/all
 // =========================
 exports.getAllUsers = async (req, res) => {
+  console.log("Fetching all users");
   try {
-    const { search } = req.query;
-    let query = { isActive: true };
-
-    if (search) {
-      query.$or = [
-        { name: { $regex: search, $options: "i" } },
-        { email: { $regex: search, $options: "i" } },
-      ];
-    }
-
-    const users = await User.find(query).select("-password");
-    res.json(users);
+    const users = await User.find({ isActive: true });
+    res.status(200).json(users);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
